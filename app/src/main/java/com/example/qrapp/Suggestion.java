ca836842fa4c46b0060062fb3b2f754ea7eff0f1
priyanshu294 <ps875761@gmail.com>
2020-08-25 16:55:11 +0530

    Added..

--- app/src/main/java/com/example/qrapp/Suggestion.java
+++ app/src/main/java/com/example/qrapp/Suggestion.java
@@ -2,14 +2,25 @@ package com.example.qrapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.util.Linkify;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.budiyev.android.codescanner.CodeScanner;
 import com.budiyev.android.codescanner.DecodeCallback;
@@ -20,13 +31,11 @@ import java.io.FileNotFoundException;
 import androidmads.library.qrgenearator.QRGContents;
 import androidmads.library.qrgenearator.QRGEncoder;
 
-public class Suggestion extends AppCompatActivity {
+public class Suggestion extends AppCompatActivity implements View.OnClickListener {
 
-    ImageView qr_code_img,delete_img,share_img,copy_img;
-    TextView content_txt,delete_txt,share_txt,copy_txt;
+    ImageView qr_code_img, delete_img, share_img, copy_img;
+    TextView content_txt, delete_txt, share_txt, copy_txt;
     Button sugg_button;
-
-
 
 
     @Override
@@ -47,7 +56,7 @@ public class Suggestion extends AppCompatActivity {
         sugg_button = findViewById(R.id.creare_btn);
 
 // content text code scan part
-        if(content_txt.getText().toString().length() > 0) {
+        if (content_txt.getText().toString().length() > 0) {
 
             content_txt.setText(getIntent().getStringExtra("MyResult"));
 
@@ -72,7 +81,84 @@ public class Suggestion extends AppCompatActivity {
             content_txt.setText(getIntent().getStringExtra("MyResult"));
         }
 
-}
+// Give suggestion for different QR code
+        IntentSuggestions();
+
+    }
+
+// calling suggestion
+    private void MakeCall() {
+        String number = content_txt.getText().toString();
+        if (number.trim().length() > 0) {
+            int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE);
+
+            if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(
+                        this,
+                        new String[]{Manifest.permission.CALL_PHONE},
+                        123);
+            } else {
+                String dial = "tel:" + number;
+                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse(dial));
+                startActivity(intent);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 123) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                MakeCall();
+            } else {
+                Toast.makeText(this, "Permission Required.", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+//  Give suggestion for different QR code
+    private void IntentSuggestions() {
+
+        if (Linkify.addLinks(content_txt, Linkify.PHONE_NUMBERS)) {
+            sugg_button.setText("Call");
+            sugg_button.setOnClickListener(this);
+        }
+    }
 
 
+    // Action bar button
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.scan_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case R.id.download:
+                // checkpermission() called and then if-else
+                // used to confirm for status of permission.
+//                checkpermission();
+//                if (!checkpermission()) {
+//                    checkpermission();
+//                } else {
+//                   // saveToGallery();
+//                }
+                break;
+
+            default:
+
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
+// For calling
+    @Override
+    public void onClick(View v) {
+        MakeCall();
+    }
 }
